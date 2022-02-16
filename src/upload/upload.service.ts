b9d9fd1412d54633b5e8f0943aaf75fd6ae8da05
B-Sallum <brusallum@hotmail.com>
2022-02-16 19:05:20 -0300

    Table ok

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,31 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import * as XLSX from 'xlsx';
-// import * as fs from 'fs';
 import { User } from '@prisma/client';
-// import { prisma } from '@prisma/client';
 import { updateTableDto } from './dto/update-preco.dto';
 import { PrismaService } from 'src/prisma.service';
-// import { TextEncoderStream } from 'node:stream/web';
 
 @Injectable()
 export class UploadService {
   constructor(private db: PrismaService) {}
-  async readFile(file: Express.Multer.File, user: User, code: string) {
+  async readFile(file: Express.Multer.File, user: User) {
     const wb = XLSX.read(file.buffer, { type: 'buffer' });
     const sheet = wb.SheetNames[0];
     const excelRows: updateTableDto[] = XLSX.utils.sheet_to_json(
       wb.Sheets[sheet],
     );
-    const updateTable = excelRows.forEach(async (product) => {
+    excelRows.forEach(async (product) => {
       const table = await this.db.product.update({
         where: { code: product.code },
         data: {
           discount: product.discount,
         },
       });
-      console.log(table, user, code);
+      console.log(table, user);
     });
-    console.log(updateTable);
-    // return updateTable
   }
 }
