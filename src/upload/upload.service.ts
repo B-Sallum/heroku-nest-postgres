8d1c089bea937571888352d8cd73ec8457d6f357
PedroCod <88497687+PedroCod@users.noreply.github.com>
2022-02-16 10:09:13 -0300

    update (#6)
    * SheetJs, Upload Table, Array table
    
    * Organization and RBAC implementation
    
    * RBAC
    
    * Mudanaçs RBAC
    
    * UpdateTable

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import * as XLSX from 'xlsx';
 import * as fs from 'fs';
-import { User } from '@prisma/client';
-import { editPrecoDto } from './dto/update-preco.dto';
+import { User, Product } from '@prisma/client';
+import { prisma } from '@prisma/client';
+import { updateTableDto } from './dto/update-preco.dto';
+import { PrismaService } from 'src/prisma.service';
+import { TextEncoderStream } from 'node:stream/web';
 
 @Injectable()
 export class UploadService {
-  async readFile(file: Express.Multer.File, user: User) {
+  constructor(private db: PrismaService) {}
+  async readFile(file: Express.Multer.File, user: User, code:string) {
     const wb = XLSX.read(file.buffer, { type: 'buffer' });
     const sheet = wb.SheetNames[0];
-    const excelRows: editPrecoDto[] = XLSX.utils.sheet_to_json(
+    const excelRows: updateTableDto[] = XLSX.utils.sheet_to_json(
       wb.Sheets[sheet],
     );
-    console.log(excelRows)
+    const updateTable = excelRows.forEach(async(excelArray) => {
+      const table = await this.db.product.update({
+        where: excelArray.code,
+        data: excelArray
+      })
+      return table
+    })
+    console.log(updateTable)
+    // return updateTable
   }
 }
