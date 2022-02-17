daae71d10d201534b66c98a040ffc52e0c192ab4
Pe Code <Pedro.hsoares131@gmail.com>
2022-02-16 23:50:14 -0300

    Mudanças de Table

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
-import { UploadModule } from './upload/upload.module';
+import { UploadModule } from './table/table.module';
 import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
